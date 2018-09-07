08355b8cc814860e08d1a3ead24addb38fd21d34
Nathan Lee <esoodev@gmail.com>
2018-09-07 13:51:53 +0900

    Removed RestaurantBusiness & edited BusinessClass to be more useful

--- src/Objects/NaverBusiness.php
+++ src/Objects/NaverBusiness.php
@@ -17,6 +17,7 @@ class NaverBusiness
                 $this->{$key} = $value;
             }
         }
+
     }
 
     public static function example()
@@ -24,38 +25,34 @@ class NaverBusiness
         return new self(null, true);
     }
 
+    public static function exampleRequiredOnly()
+    {
+        return new self(self::requiredFields(true));
+    }
+
     public static function requiredFields($setDefault = false)
     {
-        $f['businessTypeId'] = NaverDictionary::BUSINESS_TYPE_ID['숙박']; // 서비스 유형
+        $f['agencyKey'] = 'PREFIX_01234';
+        $f['name'] = '업체명'; // 업체명
         $f['serviceName'] = '서비스 이름'; // 서비스 이름
         $f['desc'] = '네이버 예약 PHP 라이브러리를 통한 업체 설명 텍스트입니다.'; // 서비스 소개
-
-        $f['businessCategory'] =
-        NaverDictionary::BUSINESS_CATEGORIES['호텔']; // 업종 구분
-        $f['email'] = 'businessemail@gmail.com'; // 업체 이메일
-        $f['name'] = '업체명'; // 업체명
+        $f['businessTypeId'] = $this->setBusinessType('식당'); // 서비스 유형
+        $f['businessCategory'] = $this->setBusinessCategory('식당'); // 업종 구분
         $f['reprOwnerName'] = '대표자명'; // 대표자명
-
+        $f['email'] = 'businessemail@gmail.com'; // 업체 이메일
         $f['addressJson']['jibun'] = '경기도 성남시 분당구 정자동 178-1';
         $f['addressJson']['posLat'] = 37.359544;
         $f['addressJson']['posLong'] = 127.105473;
-
         $f['phoneInformationJson']['wiredPhone'] = '02-123-4567'; // 업체 연락처(?)
         $f['phoneInformationJson']['reprPhone'] = '02-567-1234'; // "전화번호"
         $f['phoneInformationJson']['phoneList'] = ['02-548-0620']; // 관리자 연락처
-
-        $f['bookingConfirmCode'] = NaverDictionary::BOOKING_CONFIRM_CODES['즉시'];
-        $f['bookingTimeUnitCode'] = NaverDictionary::BOOKING_TIME_UNIT_CODES['30분'];
-        $f['uncompletedBookingProcessCode'] =
-        NaverDictionary::UNCOMPLETED_BOOKING_PROCESS_CODES['취소'];
-
-        $f['agencyKey'] = 'PREFIX_01234';
-
+        $f['bookingConfirmCode'] = $this->setBookingConfirm('즉시');
+        $f['bookingTimeUnitCode'] = $this->setBookingTimeUnit('30분');
+        $f['uncompletedBookingProcessCode'] = $this->setIncompleteBookingProcess('취소');
         $f['businessResources'][0]['resourceTypeCode'] =
         NaverDictionary::RESOURCE_TYPE_CODES['대표이미지'];
         $f['businessResources'][0]['order'] = 0;
-        $f['businessResources'][0]['resourceUrl'] = "http://c2.poing.co.kr/" .
-            "PIMAGE-original/MjAxNjA5/147513969657ecd8708a574.jpeg";
+        $f['businessResources'][0]['resourceUrl'] = '';
 
         if (!$setDefault) {
             self::_arraySetValuesNull($f);
@@ -66,10 +63,9 @@ class NaverBusiness
 
     public static function optionalFields($setDefault = false)
     {
-        $f = [];
-        // $f['bizNumber'] = '2178139965'; // 사업자 등록번호
-        // $f['cbizNumber'] = '2018-서울용산-0189'; // 통신판매업 신고번호
-        // $f['addressJson']['detail'] = '999층'; // 상세 위치
+        $f['bizNumber'] = '2178139965'; // 사업자 등록번호
+        $f['cbizNumber'] = '2018-서울용산-0189'; // 통신판매업 신고번호
+        $f['addressJson']['detail'] = '999층'; // 상세 위치
 
         if (!$setDefault) {
             self::_arraySetValuesNull($f);
@@ -78,10 +74,33 @@ class NaverBusiness
         return $f;
     }
 
-    public function toJSON()
+    /**
+     * GETTERS
+     */
+
+    public function getBusinessId()
     {
-        return json_encode(array_merge_recursive($this->requiredFields,
-            $this->optionalFields), JSON_UNESCAPED_UNICODE);
+        return $this->businessId;
+    }
+
+    public function getAddresses()
+    {
+        return $this->addressJson;
+    }
+
+    public function getPhones()
+    {
+        return $this->phoneInformationJson;
+    }
+
+    public function getBusinessName()
+    {
+        return $this->name;
+    }
+
+    public function getOwnerName()
+    {
+        return $this->reprOwnerName;
     }
 
     public function setData(array $data)
@@ -91,28 +110,29 @@ class NaverBusiness
         }
     }
 
-    public function getBusinessId()
+    public function getBusinessEmail()
     {
-        if (isset($this->businessId)) {
-            return $this->businessId;
-        }
-        return null;
+        return $this->email;
     }
 
-    public function getAddresses()
+    public function getServiceName()
     {
-        $f['addressJson'] = $this->addressJson;
-        return $f;
+        return $this->serviceName;
     }
 
-    public function setAddresses(array $newAddressJson, $returnJson = false)
+    public function getServiceDesc()
     {
-        $f['addressJson'] = $newAddressJson;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
+        return $this->desc;
     }
+
+    public function getAgencyKey()
+    {
+        return $this->agencyKey;
+    }
+
+    /**
+     * SETTERS
+     */
 
     public function setAddressJibun($newAddressJibun)
     {
@@ -129,136 +149,63 @@ class NaverBusiness
         $this->addressJson['detail'] = $newAddressDetail;
     }
 
-    public function getPhones()
+    public function setAddressCoord($posLat, $posLong)
     {
-        $f['phoneInformationJson'] = $this->phoneInformationJson;
-        return $f;
+        $this->addressJson['posLat'] = $posLat;
+        $this->addressJson['posLong'] = $posLong;
     }
 
-    public function setPhones(array $newPhoneInfoJson, $returnJson = false)
+    public function setPhoneWired($wiredPhone)
     {
-        $f['phoneInformationJson'] = $newPhoneInfoJson;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
+        $this->phoneInformationJson['wiredPhone'] = $wiredPhone;
     }
 
-    public function setPhoneWired($newPhoneWired)
+    public function setPhoneRep($reprPhone)
     {
-        $this->phoneInformationJson['wiredPhone'] = $newPhoneWired;
+        $this->phoneInformationJson['reprPhone'] = $reprPhone;
     }
 
-    public function setPhoneRep($newPhoneRep)
+    public function setPhoneList(array $phoneList)
     {
-        $this->phoneInformationJson['reprPhone'] = $newPhoneRep;
+        $this->phoneInformationJson['phoneList'] = $phoneList;
     }
 
-    public function setPhoneList(array $newPhoneList)
+    public function addPhoneList($phone)
     {
-        $this->phoneInformationJson['phoneList'] = $newPhoneList;
+        array_push($this->phoneInformationJson['phoneList'], $phone);
     }
 
-    public function addPhoneList($newPhone, $returnJson = false)
+    public function setBusinessName($name)
     {
-        array_push($this->phoneInformationJson['phoneList'], $newPhone);
-        if ($returnJson) {
-            $f['phoneInformationJson'] = $this->phoneInformationJson;
-            return $f;
-        }
+        $this->name = $name;
     }
 
-    public function getBusinessName()
+    public function setOwnerName($reprOwnerName)
     {
-        $f['name'] = $this->name;
-        return $f;
+        $this->reprOwnerName = $reprOwnerName;
     }
 
-    public function setBusinessName($newBusinessName, $returnJson = false)
+    public function setBusinessEmail($email)
     {
-        $f['name'] = $newBusinessName;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
+        $this->email = $email;
     }
 
-    public function getOwnerName()
+    public function setServiceName($serviceName)
     {
-        $f['reprOwnerName'] = $this->reprOwnerName;
-        return $f;
+        $this->serviceName = $serviceName;
     }
 
-    public function setOwnerName($newOwnerName, $returnJson = false)
+    public function setServiceDesc($desc)
     {
-        $f['reprOwnerName'] = $newOwnerName;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
+        $this->desc = $desc;
     }
 
-    public function getBusinessEmail()
+    public function setAgencyKey($agenctKey)
     {
-        $f['email'] = $this->email;
-        return $f;
+        $this->agenctKey = $agenctKey;
     }
 
-    public function setBusinessEmail($newBusinessEmail, $returnJson = false)
-    {
-        $f['email'] = $newBusinessEmail;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
-    }
-
-    public function getServiceName()
-    {
-        $f['serviceName'] = $this->serviceName;
-        return $f;
-    }
-
-    public function setServiceName($newServiceName, $returnJson = false)
-    {
-        $f['serviceName'] = $newServiceName;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
-    }
-
-    public function getServiceDesc()
-    {
-        $f['desc'] = $this->desc;
-        return $f;
-    }
-
-    public function setServiceDesc($newServiceDesc, $returnJson = false)
-    {
-        $f['desc'] = $newServiceDesc;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
-    }
-
-    public function getAgencyKey()
-    {
-        $f['agencyKey'] = $this->agencyKey;
-        return $f;
-    }
-
-    public function setAgencyKey($newAgencyKey, $returnJson = false)
-    {
-        $f['agencyKey'] = $newAgencyKey;
-        if ($returnJson) {
-            return $f;
-        }
-        $this->setData($f);
-    }
-
-    public function setMainImage($imgUrl, $returnJson = false)
+    public function setMainImage($imgUrl)
     {
         $f['businessResources'][0]['resourceTypeCode'] =
         NaverDictionary::RESOURCE_TYPE_CODES['대표이미지'];
@@ -266,6 +213,32 @@ class NaverBusiness
         $f['businessResources'][0]['resourceUrl'] = $imgUrl;
 
         $this->setData($f);
+    }
+
+    public function setBusinessType($key)
+    {
+        $this->businessTypeId = NaverDictionary::BUSINESS_TYPE_ID[$key];
+    }
+
+    public function setBusinessCategory($key)
+    {
+        $this->businessCategory = NaverDictionary::BUSINESS_CATEGORIES[$key];
+    }
+
+    public function setBookingConfirm($key)
+    {
+        $this->bookingConfirmCode = NaverDictionary::BOOKING_CONFIRM_CODES[$key];
+    }
+
+    public function setBookingTimeUnit($key)
+    {
+        $this->bookingTimeUnitCode = NaverDictionary::BOOKING_TIME_UNIT_CODES[$key];
+    }
+
+    public function setIncompleteBookingProcess($key)
+    {
+        $this->uncompletedBookingProcessCode =
+        NaverDictionary::UNCOMPLETED_BOOKING_PROCESS_CODES[$key];
     }
 
     private static function _arraySetValuesNull(&$arr)
